6cb70954903cfdbcfcfff456092f53a3232e6dc1
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2023-12-29 19:19:03 -0300

    course 2

--- imdb250/imdb250/spiders/imdb.py
+++ imdb250/imdb250/spiders/imdb.py
@@ -2,12 +2,14 @@ import scrapy
 
 
 class ImdbSpider(scrapy.Spider):
-    name = 'imdb'
-    # allowed_domains = ['imdb.com']
-    start_urls = ['http://imdb.com/']
+  name = 'imdb'
+  # allowed_domains = ['imdb.com']
+  start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
 
-    def parse(self, response):
-        filmes = response.css('.cli-children')
-        titulos = response.css('.cli-children a')
-        
-        pass
+  def parse(self, response):
+      for filmes in response.css('.cli-children'):
+        yield{
+          'titulo': filmes.css('.cli-children a').get()[118:-9],
+          'ano': filmes.css('.cli-title-metadata-item::text').get(),
+          'nota': response.css('.ratingGroup--imdb-rating, .ipc-icon--star-inline, #text::text').get()[31:34]
+        }
